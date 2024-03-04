739c72987d8832e10484afbcdedb02a492ebc712
krishanu dev <75378214+kd1995-max@users.noreply.github.com>
2024-03-04 12:05:11 +0530

    Update twosum.c

--- Array/TWO-SUM/twosum.c
+++ Array/TWO-SUM/twosum.c
@@ -1,1 +1,29 @@
+// nive
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *out = calloc(2,sizeof(int));
+    int size = 0;
+    for (int i = 0; i < numsSize; i++) {
+        
+        for ( int j = i + 1; j < numsSize; j++ ) {
 
+            if ( j >= numsSize ) {
+                break;
+            }
+
+            int sum = nums[i] + nums[j];
+
+            if( sum == target )
+            {
+                *returnSize = 2;
+                out[0] = i;
+                out[1] = j;
+                break;
+            }
+        }
+    }
+
+    return out;
+}
