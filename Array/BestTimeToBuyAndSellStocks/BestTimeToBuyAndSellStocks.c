d88187d1f3186018f350242c3a1528d496f8673b
krishanu dev <75378214+kd1995-max@users.noreply.github.com>
2024-03-07 17:10:15 +0530

    Update BestTimeToBuyAndSellStocks.c

--- Array/BestTimeToBuyAndSellStocks/BestTimeToBuyAndSellStocks.c
+++ Array/BestTimeToBuyAndSellStocks/BestTimeToBuyAndSellStocks.c
@@ -8,3 +8,24 @@ int maxProfit(int* prices, int pricesSize) {
     }
     return max_profit;
 }
+
+// striver approach 
+int min_(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int max_(int a, int b) {
+    return (a > b) ? a : b;
+}
+int maxProfit(int* prices, int pricesSize) {
+    int min_price = prices[0];
+    int max_profit = 0;
+    
+    for (int i = 1; i < pricesSize; i++) {
+        int cost = prices[i] - min_price;
+        max_profit = max_(cost, max_profit);
+
+        min_price = min_(min_price, prices[i]);
+    }
+    return max_profit;
+}
